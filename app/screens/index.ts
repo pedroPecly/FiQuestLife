4651f24a00cab8876760e56958fe5eb8ce11d824
Pedro henrique Pecly Gomes <135339009+pedroPecly@users.noreply.github.com>
2025-12-31 20:40:12 -0300

    atualizando

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -4,7 +4,6 @@ export { default as ChallengesScreen } from './ChallengesScreen';
 export { EditProfileScreen } from './EditProfileScreen';
 export { default as FriendProfileScreen } from './FriendProfileScreen';
 export { default as FriendsScreen } from './FriendsScreen';
-export { default as HealthOnboardingScreen } from './HealthOnboardingScreen';
 export { default as InventoryScreen } from './InventoryScreen';
 export { default as LoginScreen } from './LoginScreen';
 export { default as ProfileScreen } from './ProfileScreen';
